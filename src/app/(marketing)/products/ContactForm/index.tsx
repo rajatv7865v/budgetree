39b669ad5d296de90688ccd845584f796ee4d42a
rajatv7865v <rajatv785v@gmail.com>
2024-12-21 22:28:10 +0530

    final push with mail

--- src/app/(marketing)/products/ContactForm/index.tsx
+++ src/app/(marketing)/products/ContactForm/index.tsx
@@ -1,10 +1,37 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
+import axios from "axios";
 
 interface indexInterface {
   // Define your interface properties here
 }
 
 const ContactForm: React.FC<indexInterface> = () => {
+  const [value, setValue] = useState({
+    name: "",
+    email: "",
+    subject: "",
+    message: "",
+  });
+  const submitHandler = async (e: any) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post("http://localhost:4040/mail", {
+        reciepents: "support@budgetree.in",
+        subject: `${value.subject} by ${value.email}`,
+        msg: value.message,
+        phone: "",
+      });
+
+      console.log("Response:", response.data);
+    } catch (error) {
+      if (axios.isAxiosError(error)) {
+        console.error("Axios error:", error.response?.data || error.message);
+      } else {
+        console.error("Error:", error);
+      }
+    }
+  };
   return (
     <div>
       <section className='bg-white dark:bg-gray-900'>
@@ -15,7 +42,7 @@ const ContactForm: React.FC<indexInterface> = () => {
           <p className='mb-8 lg:mb-16 font-light text-center text-gray-500 dark:text-gray-400 sm:text-xl'>
             Connect to step in future of reward management.
           </p>
-          <form action='#' className='space-y-8'>
+          <form onSubmit={submitHandler} action='#' className='space-y-8'>
             <div>
               <label
                 htmlFor='fullname'
@@ -29,6 +56,8 @@ const ContactForm: React.FC<indexInterface> = () => {
                 className='shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light'
                 placeholder='Fullname'
                 required
+                onChange={(e) => setValue({ ...value, name: e.target.value })}
+                value={value.name}
               />
             </div>
             <div>
@@ -44,6 +73,8 @@ const ContactForm: React.FC<indexInterface> = () => {
                 className='shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light'
                 placeholder='name@xyz.com'
                 required
+                onChange={(e) => setValue({ ...value, email: e.target.value })}
+                value={value.email}
               />
             </div>
             <div>
@@ -59,6 +90,10 @@ const ContactForm: React.FC<indexInterface> = () => {
                 className='block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light'
                 placeholder='Let us know how we can help you'
                 required
+                onChange={(e) =>
+                  setValue({ ...value, subject: e.target.value })
+                }
+                value={value.subject}
               />
             </div>
             <div className='sm:col-span-2'>
@@ -73,6 +108,10 @@ const ContactForm: React.FC<indexInterface> = () => {
                 rows={6}
                 className='block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500'
                 placeholder='Leave a comment...'
+                onChange={(e) =>
+                  setValue({ ...value, message: e.target.value })
+                }
+                value={value.message}
               ></textarea>
             </div>
             <button
